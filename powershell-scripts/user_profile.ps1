f8d43ed6a4489e1a291dc0d763ad17df6a45b82c
Engr. Bryndell Torio <bryndell.torio@global-imi.com>
2023-09-12 06:40:14 +0800

    Added powershell-scripts folder to store all the custom ps scripts.

---
+++ powershell-scripts/user_profile.ps1
@@ -0,0 +1,42 @@
+# Reference: https://www.youtube.com/watch?v=5-aK2_WwrmM
+# prompt
+Import-Module posh-git
+Import-Module Terminal-Icons
+
+# PSReadline
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+
+# Fuzzy finder
+Import-Module PSFzf
+Set-PSFzfOption -PSReadlineChordProvide 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+oh-my-posh.exe init pwsh --config "C:\Users\bryndell.torio\.config\powershell\deltor-robruss.omp.json" | Invoke-Expression
+
+function touch {
+    [CmdletBinding(SupportsShouldProcess)]
+        param (
+                [string]$Name
+              )
+            process {
+                New-Item -ItemType File -Name $Name
+            }
+}
+
+function grep {
+    param (
+        [string]$Name
+    )
+    process {
+        findstr /si $Name
+    }
+}
+
+Set-Alias v nvim
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
