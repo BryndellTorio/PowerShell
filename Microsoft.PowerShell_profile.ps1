28d7150128036501e908d553e759dd4ec96785a4
Dell <bryndell.torio@global-imi.com>
2023-10-04 17:04:22 +0800

    Added references folder in build-project command

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -146,7 +146,7 @@ function Build-Project {
                                 "Schematic", "Layout" | ForEach-Object { New-item -Name "$_" -Path ".\$Name\Design Files\PCB" -ItemType "Directory" -ErrorAction "Ignore" }
                                 "3D models" | ForEach-Object { New-item -Name "$_" -Path ".\$Name\Design Files\PCB\Layout" -ItemType "Directory" -ErrorAction "Ignore" }
                                 "Simulation", "Design Checklist", "Test" | ForEach-Object { New-item -Name "$_" -Path ".\$Name\Design Files\Verification" -ItemType "Directory" -ErrorAction "Ignore" }
-                                "Datasheet", "Reports", "Reviews", "Meeting Notes", "Images" | ForEach-Object { New-Item -Name "$_" -Path ".\$Name\Documents" -ItemType "Directory" -ErrorAction "Ignore" }
+                                "Datasheet", "Reports", "Reviews", "Meeting Notes", "Images", "References" | ForEach-Object { New-Item -Name "$_" -Path ".\$Name\Documents" -ItemType "Directory" -ErrorAction "Ignore" }
                                 Write-Output "`n[$Name project sub-folders generated.]"
                         }
                     } else {
